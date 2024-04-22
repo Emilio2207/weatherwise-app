b5d3869a98ee0b0fdf596ac71da4a2a7321ebff4
EmilioRamirez <emi.emilioramirez@gmail.com>
2024-04-22 10:44:12 -0300

    update_22_4_final

--- app/api/pollution/route.ts
+++ app/api/pollution/route.ts
@@ -1,4 +1,4 @@
-/*import axios from "axios";
+import axios from "axios";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest) {
@@ -17,34 +17,5 @@ export async function GET(req: NextRequest) {
   } catch (error) {
     console.log("Error in getting pollusion data ", error);
     return new Response("Error fetching pollution data", { status: 500 });
-  }
-}*/
-
-import axios from "axios";
-import { NextApiRequest, NextApiResponse } from "next";
-
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  try {
-    if (!req.url) {
-      return res.status(400).json({ error: "URL is required" });
-    }
-
-    const searchParams = new URLSearchParams(req.url.split("?")[1]);
-    const lat = searchParams.get("lat");
-    const lon = searchParams.get("lon");
-
-    if (!lat || !lon) {
-      return res.status(400).json({ error: "Latitude and longitude are required" });
-    }
-
-    const apiKey = process.env.OPENWEATHERMAP_API_KEY;
-    const url = `http://api.openweathermap.org/data/2.5/air_pollution?lat=${lat}&lon=${lon}&appid=${apiKey}`;
-
-    const response = await axios.get(url);
-
-    return res.status(200).json(response.data);
-  } catch (error) {
-    console.error("Error in getting pollution data", error);
-    return res.status(500).json({ error: "Error fetching pollution data" });
   }
 }
